74426302b6a0bd11151f38a04ac842069e850332
BuildTools <cybergoose13@gmail.com>
2020-12-30 04:04:39 -0800

    playing with c more

--- HelloFriend/HelloFriend/main.c
+++ HelloFriend/HelloFriend/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "myHeader.h"
+
+
+void loop(int loops) {
+    if (myConditional(loops)) {
+        for (int i = 0; i < loops; i++) {
+            printf("Loop %d\n", i);
+        }
+    }
+    else {
+        printf("I wont loop if more than 10");
+    }
+}
+
+int myConditional(int input) {
+    if (input >= 10) return false;
+    return true;
+}
 
 int main(){
+    int a, b;
     printf("Hello Friend\n");
+    printf("Enter First Number:\n");
+    scanf_s("%d", &a);
+    printf("Enter Second Number:\n");
+    scanf_s("%d", &b);
+    int x = sum(a, b);
+    printf("My name is %s\n",name());
+    loop(x);
     return 0;
 }
---
+++ HelloFriend/HelloFriend/myHeader.h
@@ -0,0 +1,8 @@
+#pragma once
+int sum(int a, int b) {
+	return (a + b);
+}
+
+char* name() {
+	return "Cyber Goose";
+}
